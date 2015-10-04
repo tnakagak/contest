88a55f93909033f4471a48de529d374b1c5fad3b
tnakagak <nakagakitakashi@gmail.com>
2015-10-04 20:24:23 +0900

    スタック

---
+++ chap2/chap2_1/stack.py
@@ -0,0 +1,14 @@
+# coding:utf-8
+
+stack = []
+
+stack.append(1)
+stack.append(2)
+stack.append(3)
+print stack[-1]
+stack.pop()
+print stack[-1]
+stack.pop()
+print stack[-1]
+stack.pop()
+print stack
